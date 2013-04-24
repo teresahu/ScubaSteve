5b37bcc0822f703029c5b095e375ef504f92434b
Chip Cely <chip4@gatech.edu>
2013-04-24 16:34:36 -0400

    added dive timer

---
+++ Assets/Scripts/HUD.js
@@ -0,0 +1,34 @@
+#pragma strict
+
+var oxygenTime = 3;//in minutes
+var diveTime = 1;//in minutes
+var alarmSound: AudioSource;
+
+function Start () {
+	var aSources = GetComponents(AudioSource);
+	alarmSound = aSources[0];
+}
+
+function Update(){
+	if(!alarmSound.isPlaying && 
+			(Input.GetKeyDown("t") ||
+			diveTime*60 <= Time.realtimeSinceStartup))
+		alarmSound.Play();
+	if(Input.GetKeyDown("k"))
+		alarmSound.Stop();
+}
+
+function OnGUI(){
+	var currentTime = Time.realtimeSinceStartup;
+	var diveTimeRemaining = diveTime*60-currentTime;
+	if(diveTimeRemaining <= 0){
+		diveTimeRemaining = 0;
+	}
+	GUI.Label(Rect(0,0,Screen.width,Screen.height),
+		   "Depth: "+ -Camera.mainCamera.gameObject.transform.position.y+
+		   "\nOxygen level:"+(100*(oxygenTime*60-currentTime)/(oxygenTime*60)).ToString("n1")+"%"+
+		   "\nTime:"+currentTime+"%"+
+		   "\nDive Timer:"+diveTimeRemaining.ToString("n0")+" secs"
+		   );
+}
+
