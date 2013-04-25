f453d9754efca133b45eb7a1af283889fc247eba
Chip Cely <chip4@gatech.edu>
2013-04-25 11:35:58 -0400

    added water and fog only on underwater

--- Assets/Scripts/Underwater.js
+++ Assets/Scripts/Underwater.js
@@ -1,36 +1,13 @@
-//This script enables underwater effects. Attach to main camera.
- 
-//Define variables
-var underwaterLevel = 3;
-private var defaultFog;
-private var defaultFogColor;
-private var defaultFogDensity;
-private var defaultSkybox;
- 
-//The scene's default fog settings
-var noSkybox : Material;
- 
+#pragma strict
+
 function Start () {
-	defaultFog = RenderSettings.fog;
-	defaultFogColor = RenderSettings.fogColor;
-	defaultFogDensity = RenderSettings.fogDensity;
-	defaultSkybox = RenderSettings.skybox;
-	//Set the background color
-	camera.backgroundColor = Color (0, 0.4, 0.7, 1);
+
 }
- 
+
 function Update () {
-	if (transform.position.y < underwaterLevel) {
+	if(Camera.mainCamera.gameObject.transform.position.y >=0){
+		RenderSettings.fog = false;
+	}else{
 		RenderSettings.fog = true;
-		RenderSettings.fogColor = Color (0, 0.4, 0.7, 0.6);
-		RenderSettings.fogDensity = 0.04;
-		RenderSettings.skybox = noSkybox;
-	}
- 
-	else {
-		RenderSettings.fog = defaultFog;
-		RenderSettings.fogColor = defaultFogColor;
-		RenderSettings.fogDensity = defaultFogDensity;
-		RenderSettings.skybox = defaultSkybox;
 	}
 }
